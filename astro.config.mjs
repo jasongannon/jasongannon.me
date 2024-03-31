b9ed272efe12f14553a8ff48513955754a47ba42
jasongannon <jason.gannon@protonmail.com>
2024-03-31 14:04:49 -0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import rehypeExternalLinks from 'rehype-external-links';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://jasongannon.me',
+  integrations: [tailwind(), sitemap()],
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  }
 });
