461ed0cfea38f27017139114dbf8a815995ac576
jasongannon <jason.gannon@protonmail.com>
2024-04-16 21:45:58 -0400

    i forget

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from 'rehype-external-links';
-
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasongannon.me',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon(), mdx()],
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
       content: {
