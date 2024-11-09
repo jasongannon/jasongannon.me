0f63430a465507317fcc4e7b27ccccbf14b7b3b5
jasongannon <102169615+jasgannon@users.noreply.github.com>
2024-11-09 02:09:50 -0500

    ohyeah

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,22 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasongannon.me',
-  integrations: [tailwind(), sitemap(), icon(), mdx()],
+  integrations: [tailwind(), sitemap(), icon(), mdx({
+    // Ensure MDX uses the same configuration
+    syntaxHighlight: 'prism',
+    gfm: true,
+    wrap: false
+  })],
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
       content: {
         type: 'text',
         value: ' 🔗'
       }
-    }]]
+    }]],
+    syntaxHighlight: 'prism',
+    // Add this wrap configuration
+    gfm: true,
+    wrap: false    
   }
 });
